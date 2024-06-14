7e181011006c2733da11d8abb3207c7baf46bd1a
Antonio Paolillo <apaolill@gmail.com>
2024-06-14 23:46:35 +0200

    tests: add test campaigns for perf & flamegraphs
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- benchkit/utils/git.py
+++ benchkit/utils/git.py
@@ -12,13 +12,27 @@ import git
 def clone_repo(
     repo_url: str,
     repo_src_dir: PathType,
-    tag: str,
+    tag: str = "",
+    commit: str = "",
 ) -> None:
-    """Clone given repository in the given directory and point to the given tag."""
+    """Clone given repository in the given directory and point to the given tag or the given commit."""
+
+    if tag and commit:
+        raise ValueError("tag and commit cannot be specified at the same time")
+
     if not repo_src_dir.is_dir():
         os.makedirs(repo_src_dir, exist_ok=False)
-        git.Repo.clone_from(
-            url=repo_url,
-            to_path=repo_src_dir,
-            branch=tag,
-        )
+
+        if tag:
+            repo = git.Repo.clone_from(
+                url=repo_url,
+                to_path=repo_src_dir,
+                branch=tag,
+            )
+        else:
+            repo = git.Repo.clone_from(
+                url=repo_url,
+                to_path=repo_src_dir,
+            )
+        if commit:
+            repo.git.checkout(commit)
