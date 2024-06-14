64b518c5ca829086fb324822c901dbd1af9ad9ef
Antonio Paolillo <apaolill@gmail.com>
2024-06-14 17:24:49 +0200

    Add tests for ltrace & strace (#56)
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- examples/graph500/kit/graph500.py
+++ examples/graph500/kit/graph500.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2024 Vrije Universiteit Brussel. All rights reserved.
+# SPDX-License-Identifier: MIT
 """
 Benchkit support for the Graph500 benchmark.
 See: https://github.com/graph500/graph500
