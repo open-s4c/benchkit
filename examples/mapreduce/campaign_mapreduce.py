7f4646bf216cef06262a79e7bbceecf3cd597fb8
Antonio Paolillo <apaolill@gmail.com>
2024-06-18 19:06:02 +0200

    examples: small fixes for mapreduce
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- examples/mapreduce/campaign_mapreduce.py
+++ examples/mapreduce/campaign_mapreduce.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+# Copyright (C) 2024 Vrije Universiteit Brussel. All rights reserved.
+# SPDX-License-Identifier: MIT
 
 """
 Example of campaign script for Metis/MapReduce benchmark.
 """
+
+import sys
 
 from mapreduce import metis_campaign
 
@@ -14,7 +18,21 @@ def main() -> None:
     """Main function of the campaign script."""
     metis_src_dir = (get_curdir(__file__) / "deps/Metis/").resolve()
 
-    campaign = metis_campaign(post_run_hooks=[],command_wrappers=[], src_dir=metis_src_dir)
+    if not metis_src_dir.is_dir():
+        print(
+            (
+                "Please download Metis first:\n"
+                "  cd examples/mapreduce\n"
+                "  mkdir deps/\n"
+                "  cd deps/\n"
+                "  git clone https://github.com/ydmao/Metis.git\n"
+                "  git checkout e5b04e2aa53301de71f0f5193f36e88c82008e6f"
+            ),
+            file=sys.stderr,
+        )
+        exit(1)
+
+    campaign = metis_campaign(src_dir=metis_src_dir)
 
     campaigns = [campaign]
     suite = CampaignSuite(campaigns=campaigns)
---
+++ examples/mapreduce/kit/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Vrije Universiteit Brussel. All rights reserved.
+# SPDX-License-Identifier: MIT
