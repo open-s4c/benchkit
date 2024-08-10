e9bcf281dd7c0c31eb5ebc302a762c2f7e23242d
Antonio Paolillo <apaolill@gmail.com>
2024-08-10 17:23:18 +0200

    scripts: small fixes for the new add path venv (#74)
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- scripts/add_paths_venv.py
+++ scripts/add_paths_venv.py
@@ -3,24 +3,47 @@
 Adds the paths specified in dependency-paths.txt to the benchkit-dep.pth files
 in the site-packages directory.
 """
-import os
 import sys
 from pathlib import Path
 
-target_file = "benchkit-dep.pth"
-venv_path = sys.argv[1] if len(sys.argv) < 2 else "venv"
 
-venv_site_packages_path = next(Path(f"{venv_path}/lib").glob("**/site-packages/")).resolve()
+def relative_to(path_to_return: Path, relto_path: Path) -> Path:
+    if sys.version_info >= (3, 12):
+        result = path_to_return.resolve().relative_to(relto_path, walk_up=True)
+    else:
+        import os
+        result = os.path.relpath(
+            os.path.realpath(path_to_return),
+            os.path.realpath(relto_path),
+        )
+    return result
 
-venv_rel_paths = []
-with open("./dependency-paths.txt") as dependency_types_file:
-    for line in dependency_types_file:
-        line_path = Path(line.rstrip())
-        if line_path.is_dir():
+
+def main() -> None:
+    target_filename = "benchkit-dep.pth"
+    dep_filepath = Path("./dependency-paths.txt").resolve()
+    venv_path = sys.argv[1] if len(sys.argv) >= 2 else "venv"
+
+    venv_site_packages_path = next(Path(f"{venv_path}/lib").glob("**/site-packages/")).resolve()
+
+    venv_rel_paths = []
+
+    if dep_filepath.is_file():
+        with open(dep_filepath) as dependency_types_file:
+            lines = dependency_types_file.readlines()
+        for line in lines:
+            line_path = Path(line.rstrip())
+            if not line_path.is_dir():
+                print(f"[WARNING] {line_path} is not a directory", file=sys.stderr)
+                continue
             # get the path relative to the site-packages path
-            venv_rel_path = line_path.resolve().relative_to(venv_site_packages_path, walk_up=True)
+            venv_rel_path = relative_to(path_to_return=line_path.resolve(), relto_path=venv_site_packages_path)
             venv_rel_paths.append(venv_rel_path)
 
-with open(os.path.join(venv_site_packages_path, target_file), "a") as file:
-    file.writelines(map(lambda p: str(p) + '\n', venv_rel_paths))
+    target_path = venv_site_packages_path / target_filename
+    with open(target_path, "a") as file:
+        file.writelines(map(lambda p: str(p) + '\n', venv_rel_paths))
 
+
+if __name__ == "__main__":
+    main()
