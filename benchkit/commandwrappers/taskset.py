5cf4ade19ff9ba46a5bb46cfe212b3b6b6b187a3
Rafael Lourenço de Lima Chehab <chehabrafael@gmail.com>
2024-08-21 14:23:39 +0200

    Allow pinning CPUs using TaksetWrap (#85)
    Add option of `set_all_cpus` which sets the CPU affinity for all threads
    running.
    This requires that the benchmark being used passes `nb_threads` to the
    command_wrapper.
    
    If `set_all_cpus` is not set or `nb_threads` is not given, then keeps
    previous behavior -- that is, pinning only the master thread.
    
    Signed-off-by: Rafael Chehab <chehabrafael@gmail.com>

--- benchkit/commandwrappers/taskset.py
+++ benchkit/commandwrappers/taskset.py
@@ -16,9 +16,14 @@ from . import CommandWrapper, PackageDependency
 class TasksetWrap(CommandWrapper):
     """Command wrapper for the `taskset` utility."""
 
-    def __init__(self, platform: Platform | None = None):
+    def __init__(
+            self,
+            platform: Platform | None = None,
+            set_all_cpus: bool = False,
+        ):
         super().__init__()
         self.platform = platform if platform is not None else get_current_platform()
+        self.set_all_cpus = set_all_cpus
 
     def dependencies(self) -> List[PackageDependency]:
         return super().dependencies() + [
@@ -29,6 +34,7 @@ class TasksetWrap(CommandWrapper):
         self,
         cpu_order: CpuOrder = None,
         master_thread_core: Optional[int] = None,
+        nb_threads: Optional[int] = None,
         **kwargs,
     ) -> List[str]:
         cmd_prefix = super().command_prefix(
@@ -39,15 +45,27 @@ class TasksetWrap(CommandWrapper):
 
         mtc = master_thread_core
 
-        if mtc is None:
-            if cpu_order is None:
+        if self.set_all_cpus:
+            if cpu_order is None or nb_threads is None:
                 return []
 
             cpu_order_list = self.platform.cpu_order(provided_order=cpu_order)
 
-            mtc = self.platform.master_thread_core_id(
-                cpu_order_list=cpu_order_list,
-            )
+            cpu_order_list = [str(x) for x in cpu_order_list[0:nb_threads]]
+            cpu_order_str = ','.join(cpu_order_list)
 
-        cmd_prefix = ["taskset", "--cpu-list", str(mtc)] + cmd_prefix
+            cmd_prefix = ["taskset", "--cpu-list", cpu_order_str] + cmd_prefix
+        else:
+            if mtc is None:
+                if cpu_order is None:
+                    return []
+
+                cpu_order_list = self.platform.cpu_order(provided_order=cpu_order)
+
+                mtc = self.platform.master_thread_core_id(
+                    cpu_order_list=cpu_order_list,
+                )
+
+            cmd_prefix = ["taskset", "--cpu-list", str(mtc)] + cmd_prefix
+
         return cmd_prefix
--- examples/leveldb/kit/leveldb.py
+++ examples/leveldb/kit/leveldb.py
@@ -218,6 +218,7 @@ class LevelDBBench(Benchmark):
             environment=environment,
             cpu_order=cpu_order,
             master_thread_core=master_thread_core,
+            nb_threads=nb_threads,
             **kwargs,
         )
 
