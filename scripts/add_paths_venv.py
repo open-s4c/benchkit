685c9003989f992fed766fd104984ac37f6fdbd5
Antonio Paolillo <apaolill@gmail.com>
2024-08-21 14:48:23 +0200

    venv: systematically add benchkit as a dependency
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- scripts/add_paths_venv.py
+++ scripts/add_paths_venv.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Adds the paths specified in dependency-paths.txt to the benchkit-dep.pth files
 in the site-packages directory.
@@ -12,6 +12,7 @@ def relative_to(path_to_return: Path, relto_path: Path) -> Path:
         result = path_to_return.resolve().relative_to(relto_path, walk_up=True)
     else:
         import os
+
         result = os.path.relpath(
             os.path.realpath(path_to_return),
             os.path.realpath(relto_path),
@@ -19,30 +20,43 @@ def relative_to(path_to_return: Path, relto_path: Path) -> Path:
     return result
 
 
+def get_benchkit_path() -> Path:
+    this_script = Path(__file__)
+    script_dir = this_script.parent
+    benchkit_dir = script_dir.parent
+    result = benchkit_dir.resolve()
+    return result
+
+
 def main() -> None:
     target_filename = "benchkit-dep.pth"
     dep_filepath = Path("./dependency-paths.txt").resolve()
-    venv_path = sys.argv[1] if len(sys.argv) >= 2 else "venv"
+    venv_path = Path(sys.argv[1] if len(sys.argv) >= 2 else "venv")
 
-    venv_site_packages_path = next(Path(f"{venv_path}/lib").glob("**/site-packages/")).resolve()
+    if not venv_path.is_dir():
+        print(f"No venv directory found in: {venv_path}", file=sys.stderr)
+        exit(1)
 
-    venv_rel_paths = []
+    venv_site_packages_path = next(Path(venv_path / "lib").glob("**/site-packages/")).resolve()
+
+    venv_abs_paths = [get_benchkit_path()]
 
     if dep_filepath.is_file():
         with open(dep_filepath) as dependency_types_file:
-            lines = dependency_types_file.readlines()
+            lines = [sline for line in dependency_types_file.readlines() if (sline := line.strip())]
         for line in lines:
-            line_path = Path(line.rstrip())
-            if not line_path.is_dir():
-                print(f"[WARNING] {line_path} is not a directory", file=sys.stderr)
-                continue
-            # get the path relative to the site-packages path
-            venv_rel_path = relative_to(path_to_return=line_path.resolve(), relto_path=venv_site_packages_path)
-            venv_rel_paths.append(venv_rel_path)
+            line_path = Path(line)
+            if line_path.is_dir():
+                venv_abs_path = line_path.resolve()
+                venv_abs_paths.append(venv_abs_path)
+            else:
+                print(f"[WARNING] '{line_path}' is not a directory, skipping.", file=sys.stderr)
+
+    venv_abs_paths = list(dict.fromkeys(venv_abs_paths))  # remove duplicates
 
     target_path = venv_site_packages_path / target_filename
-    with open(target_path, "a") as file:
-        file.writelines(map(lambda p: str(p) + '\n', venv_rel_paths))
+    with open(target_path, "w") as file:
+        file.writelines(f"{p}\n" for p in venv_abs_paths)
 
 
 if __name__ == "__main__":
