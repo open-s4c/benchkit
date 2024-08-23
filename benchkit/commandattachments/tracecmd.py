ebfc4059a5c56e1f8d02726df65f4249182240bd
Mats Van Molle <73903958+matsvanmolle@users.noreply.github.com>
2024-08-23 09:28:28 +0200

    resolved rest of comments

--- benchkit/commandattachments/tracecmd.py
+++ benchkit/commandattachments/tracecmd.py
@@ -1,23 +1,25 @@
 # Copyright (C) 2024 Vrije Universiteit Brussel. All rights reserved.
 # SPDX-License-Identifier: MIT
 
+from typing import List
 from benchkit.shell.shellasync import AsyncProcess
 from benchkit.utils.types import PathType
-from benchkit.platforms import get_current_platform
+from benchkit.platforms import get_current_platform, Platform
 import pathlib
 
 
 class TraceCmd:
 
-    def __init__(self, events: list[str] = []):
-        self.events = events
-
+    def __init__(self, events: List[str] = (), platform: Platform = None):
+        self.events = [str(e) for e in events]
+        self.platform = platform
     def attachement(
         self,
         process: AsyncProcess,
         record_data_dir: PathType,
     ) -> None:
-        platform = get_current_platform()
+        if self.platform == None:
+            get_current_platform()
 
         rdd = pathlib.Path(record_data_dir)
         out_file = rdd / "trace.dat"
