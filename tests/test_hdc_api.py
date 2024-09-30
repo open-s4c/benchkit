a632b0f835b6551f63818ee2ecbdd30909eb14a7
Antonio Paolillo <apaolill@gmail.com>
2024-09-30 19:26:58 +0200

    hdc: reformat file
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- tests/test_hdc_api.py
+++ tests/test_hdc_api.py
@@ -5,11 +5,13 @@ Module to test the HDC api basics
 """
 
 import os
+
 from benchkit.hdc import *
 
 HOST_FILE = ".gitignore"
 TMP_FILE = "cloned-gitignore"
 TARGET_FILE = "/storage/media/100/local/testing/.gitignore"
+
 
 def main() -> None:
     device = OpenHarmonyDeviceConnector.query_devices(lambda _: True)[0]
@@ -22,13 +24,13 @@ def main() -> None:
     bridge.push(HOST_FILE, TARGET_FILE)
     bridge.pull(TARGET_FILE, TMP_FILE)
 
-    f = open(TMP_FILE, 'r')
-    content=f.read()
+    f = open(TMP_FILE, "r")
+    content = f.read()
     print(content)
     f.close()
 
     os.remove(f.name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
