e4ae0d944fda4bf8cc39a7bd0fad5ee57d4a9cda
Antonio Paolillo <apaolill@gmail.com>
2024-11-12 11:56:01 +0100

    ipc example: apply style (#137)
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- examples/ipc/ipc.py
+++ examples/ipc/ipc.py
@@ -1,17 +1,16 @@
 import pathlib
-from typing import Any, Iterable, List, Dict
+import re
+from enum import Enum
+from typing import Any, Dict, Iterable, List
 
 from benchkit.benchmark import Benchmark, CommandAttachment, PostRunHook, PreRunHook
-from benchkit.campaign import CampaignSuite, CampaignIterateVariables, Campaign
+from benchkit.campaign import Campaign, CampaignIterateVariables, CampaignSuite
 from benchkit.commandwrappers import CommandWrapper
+from benchkit.hdc import OpenHarmonyDeviceConnector
 from benchkit.platforms import Platform, get_current_platform
 from benchkit.sharedlibs import SharedLib
-from benchkit.utils.types import PathType
 from benchkit.utils.dir import caller_dir
-from benchkit.hdc import OpenHarmonyDeviceConnector
-from enum import Enum
-import re
-
+from benchkit.utils.types import PathType
 
 BUILD_VARIABLES = []
 RUN_VARIABLES = ["m"]
@@ -54,38 +53,33 @@ class IPCBenchmark(Benchmark):
 
         if hdc is not None:
             self.hdc = hdc
-    
 
     @property
     def bench_src_path(self) -> pathlib.Path:
         return self.bench_dir
-    
 
     @staticmethod
     def get_build_var_names() -> List[str]:
         return BUILD_VARIABLES
-    
 
     @staticmethod
     def get_run_var_names() -> List[str]:
         return RUN_VARIABLES
-    
 
     @staticmethod
     def get_tilt_var_names() -> List[str]:
         return TILT_VARIABELS
-    
 
     def parse_output_to_results(
-        self, 
-        command_output: str, 
-        build_variables: Dict[str, Any], 
-        run_variables: Dict[str, Any], 
-        benchmark_duration_seconds: int, 
-        record_data_dir: PathType, 
-        **kwargs
+        self,
+        command_output: str,
+        build_variables: Dict[str, Any],
+        run_variables: Dict[str, Any],
+        benchmark_duration_seconds: int,
+        record_data_dir: PathType,
+        **kwargs,
     ) -> Dict[str, Any]:
-        raw_output = command_output[command_output.index("Successfully roundtrip"):]
+        raw_output = command_output[command_output.index("Successfully roundtrip") :]
         m = re.search(pattern=r"took:\s*([\d.]+)s", string=raw_output)
         if m is None:
             raise ValueError(f"Failed to parse output: '{raw_output}'")
@@ -98,13 +92,13 @@ class IPCBenchmark(Benchmark):
     def build_bench(self, **kwargs) -> None:
         if self.mobile:
             return
-        
+
         self.platform.comm.shell(
             command="cargo build",
             current_dir=self.bench_dir,
             output_is_log=True,
         )
-    
+
     def clean_bench(self) -> None:
         if self.mobile:
             return
@@ -115,22 +109,18 @@ class IPCBenchmark(Benchmark):
                 current_dir=self.bench_dir,
                 output_is_log=True,
             )
-    
+
     def single_run(self, m: int, **kwargs) -> str:
         run_command: List[str]
         output: str
 
         if self.mobile:
-            run_command = [
-                "./ipc_runner", "-m", f"{m}"
-            ]
+            run_command = ["./ipc_runner", "-m", f"{m}"]
             # TODO: maybe wrap as well or don't and make comm layer
             output = self.hdc.shell_out(run_command, self.bench_dir)
             print(output)
         else:
-            run_command = [
-                "cargo", "run", "--", "-m", f"{m}"
-            ]
+            run_command = ["cargo", "run", "--", "-m", f"{m}"]
 
             wrapped_run_command, wrapped_environment = self._wrap_command(
                 run_command=run_command,
@@ -152,7 +142,7 @@ class IPCBenchmark(Benchmark):
 
 def main() -> None:
     nb_runs = 2
-    variables = [{ "m": 10 ** i } for i in range(1, 4)]
+    variables = [{"m": 10**i} for i in range(1, 4)]
     skip_rebuild = False
     target = Target.LOCAL
 
@@ -170,9 +160,10 @@ def main() -> None:
             hdc = OpenHarmonyDeviceConnector.query_devices(lambda _: True)[0]
         case Target.CONTAINER:
             from rustcontainer import get_rust_docker_platform
+
             platform = get_rust_docker_platform(host_dir=this_dir)
             bench_dir = "/home/user/workspace/mnt/ipc_runner"
-    
+
     benchmark = IPCBenchmark(
         bench_dir=bench_dir,
         platform=platform,
