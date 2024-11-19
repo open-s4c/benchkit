beb1dcfd762303b77553797d9f9e8335b2128d85
Rafael Chehab <chehabrafael@gmail.com>
2024-11-19 18:50:35 +0100

    Fix cloudsuite.py copyright
    Signed-off-by: Rafael Chehab <chehabrafael@gmail.com>

--- examples/cloudsuite/kit/cloudsuite.py
+++ examples/cloudsuite/kit/cloudsuite.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Huawei Technologies Co., Ltd. All rights reserved.
+# Copyright (C) 2024 Huawei Technologies Co., Ltd. All rights reserved.
 # SPDX-License-Identifier: MIT
 
 import pathlib
