d4a45eb2668a07b7cde5474e4c864da1857cddb3
Antonio Paolillo <apaolill@gmail.com>
2024-11-23 18:08:55 +0100

    example/tilt: factor out simple test benchmark (#144)
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- examples/tilt/kit/tiltlib.py
+++ examples/tilt/kit/tiltlib.py
@@ -3,11 +3,12 @@
 
 import pathlib
 import sys
-from typing import Tuple
+from typing import Any, Dict, Iterable, List, Tuple
 
+from benchkit.benchmark import Benchmark, SharedLib
 from benchkit.platforms import Platform
 from benchkit.sharedlibs import FromSourceSharedLib
-from benchkit.utils.types import EnvironmentVariables, LdPreloadLibraries
+from benchkit.utils.types import EnvironmentVariables, LdPreloadLibraries, PathType
 
 
 def cmake_configure_build(
@@ -75,3 +76,94 @@ class TiltLib(FromSourceSharedLib):
                 )
 
         return ld_preloads, env_vars
+
+
+class SimpleMutexTestBench(Benchmark):
+    def __init__(
+        self,
+        src_dir: PathType,
+        shared_libs: Iterable[SharedLib] = (),
+        platform: Platform = None,
+    ) -> None:
+        super().__init__(
+            command_wrappers=(),
+            command_attachments=(),
+            shared_libs=shared_libs,
+            pre_run_hooks=(),
+            post_run_hooks=(),
+        )
+        if platform is not None:
+            self.platform = platform
+        self._src_dir = src_dir.resolve()
+        self._build_dir = self._src_dir / f"build-{self.platform.hostname}"
+
+    @property
+    def bench_src_path(self) -> pathlib.Path:
+        return self._src_dir
+
+    @staticmethod
+    def get_build_var_names() -> List[str]:
+        return []
+
+    @staticmethod
+    def get_run_var_names() -> List[str]:
+        return ["lock"]
+
+    def clean_bench(self) -> None:
+        pass
+
+    def prebuild_bench(
+        self,
+        **kwargs,
+    ) -> int:
+        cmake_configure_build(
+            platform=self.platform,
+            src_dir=self.bench_src_path,
+            build_dir=self._build_dir,
+            debug=self.must_debug(),
+            make_suffix=self._parallel_make_str(),
+        )
+        return 0
+
+    def build_bench(
+        self,
+        **kwargs,
+    ) -> None:
+        pass
+
+    def single_run(
+        self,
+        **kwargs,
+    ) -> str:
+        current_dir = self._build_dir
+        environment = self._preload_env(
+            **kwargs,
+        )
+
+        run_command = ["./mutex"]
+
+        wrapped_run_command, wrapped_environment = self._wrap_command(
+            run_command=run_command,
+            environment=environment,
+            **kwargs,
+        )
+
+        output = self.run_bench_command(
+            run_command=run_command,
+            wrapped_run_command=wrapped_run_command,
+            current_dir=current_dir,
+            environment=environment,
+            wrapped_environment=wrapped_environment,
+            print_output=False,
+            ignore_ret_codes=(1,),
+        )
+        print(output)
+        return output
+
+    def parse_output_to_results(  # pylint: disable=arguments-differ
+        self,
+        command_output: str,
+        **_kwargs,
+    ) -> Dict[str, Any]:
+        result_dict = {}
+        return result_dict
