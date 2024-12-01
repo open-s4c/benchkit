ea7ee1e01971bd7e4421a307a043ef1c727b8d9f
Antonio Paolillo <apaolill@gmail.com>
2024-12-01 15:43:48 +0100

    plotbench: automatically install venv (#154)
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- plotbench/plotbench.sh
+++ plotbench/plotbench.sh
@@ -3,6 +3,15 @@ set -e
 
 script_dir=$(readlink -e "$(dirname "$0")")
 py3=${script_dir}/venv/bin/python3
+
+if [ ! -e "${py3}" ]
+then
+  (
+    cd "${script_dir}"
+    ./configure.sh
+  )
+fi
+
 webapp=${script_dir}/src/webapp.py
 cur_dir=${PWD}
 
