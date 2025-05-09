6d1231cdaf4a046bd2b17f31c27427267d4b813d
Antonio Paolillo <apaolill@gmail.com>
2025-05-09 18:59:10 +0200

    examples/gpus: add debug mode for libsmctrl
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- examples/gpus/kit/smctrl.py
+++ examples/gpus/kit/smctrl.py
@@ -9,6 +9,7 @@ def install_libsmctrl_from_src(
     builder: DockerBuilder,
     workdir: str,
     smctrl_commit: str = "72ba87e277572eddb25784563faa3eac111c9556",
+    debug=True,
 ) -> None:
     builder.desc("Install libsmctrl from source")
     project_git_clone(
@@ -18,10 +19,11 @@ def install_libsmctrl_from_src(
         commit=smctrl_commit,
         single_run_command=True,
     )
+    flags = ' CFLAGS="-g -O0"' if debug else ""
     builder.run_multiple(
         commands=[
             f"cd {workdir}/libsmctrl",
-            "make -j$(nproc) NVCC=$(which nvcc) libsmctrl.a",
+            f'make -j$(nproc) NVCC=$(which nvcc){flags} libsmctrl.a',
         ],
     )
     builder.space()
