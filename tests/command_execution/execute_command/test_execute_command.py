1f737cf39196cf862797fb9b79ec0ae369fc3f06
Bogaert Aaron <aaron.bogaert@vub.be>
2025-06-10 15:48:22 +0200

    finish the basic execute tests
    Signed-off-by: Bogaert Aaron <aaron.bogaert@vub.be>

--- tests/command_execution/execute_command/test_execute_command.py
+++ tests/command_execution/execute_command/test_execute_command.py
@@ -1,13 +1,14 @@
 import itertools
 import os
 import pathlib
+from subprocess import CalledProcessError
 from typing import Any, Dict, List, Optional, Tuple
 import unittest
 
-from benchkit.shell.CommunicationLayer.IO_stream import ReadableIOStream, WritableIOStream
-from benchkit.shell.CommunicationLayer.OutputObject import Output
-from benchkit.shell.CommunicationLayer.hooks.basic_hooks import create_stream_line_logger_hook, create_voiding_result_hook, logger_line_hook, std_out_result_void_err, void_hook
+from benchkit.shell.CommunicationLayer.IO_stream import ReadableIOStream, StringIOStream, WritableIOStream
+from benchkit.shell.CommunicationLayer.hooks.basic_hooks import create_stream_line_logger_hook, create_voiding_result_hook, logger_line_hook, void_hook
 from benchkit.shell.CommunicationLayer.hooks.hook import IOHook, IOResultHook, IOWriterHook, OutputHook
+from shell_scripts import TestTimeout, script_path_string, timeout
 from benchkit.shell.ast_shell_out import execute_command
 
 def get_arguments_dict_list(
@@ -106,22 +107,27 @@ class BasicShellTests(unittest.TestCase):
             # hook based argumens
             hooklist = generate_test_hook_lists(force_output=True)
             for input_hooks,output_hooks,result_hook_object in hooklist:
-
+                try:
                 # execution
-                execute_command(
-                        ["echo", "benchkit_echo_test", str(arguments)],
-                        ordered_output_hooks=output_hooks,
-                        ordered_input_hooks=input_hooks,
-                        **arguments,
-                    )
+                    with timeout(5):
+                        execute_command(
+                                ["echo", "benchkit_echo_test", str(arguments)],
+                                ordered_output_hooks=output_hooks,
+                                ordered_input_hooks=input_hooks,
+                                **arguments,
+                            )
 
-                # result gathering
-                output = result_hook_object.get_result()
-                expected_output = f"benchkit_echo_test {str(arguments)}\n".encode("utf-8")
-                self.assertEqual(
-                        output,
-                        expected_output,
-                        "shell does not provide the right output in the result",
+                        # result gathering
+                        output = result_hook_object.get_result()
+                        expected_output = f"benchkit_echo_test {str(arguments)}\n".encode("utf-8")
+                        self.assertEqual(
+                                output,
+                                expected_output,
+                                "shell does not provide the right output in the result",
+                            )
+                except TestTimeout:
+                    self.fail(
+                        "execution timed out"
                     )
 
     @unittest.skip("disabled for debugging")
@@ -142,45 +148,59 @@ class BasicShellTests(unittest.TestCase):
             # hook based argumens
             hooklist = generate_test_hook_lists(force_output=True)
             for input_hooks,output_hooks,result_hook_object in hooklist:
-
+                try:
                 # execution
-                execute_command(
-                        ["env"],
-                        ordered_output_hooks=output_hooks,
-                        ordered_input_hooks=input_hooks,
-                        **arguments,
-                    )
-                env = result_hook_object.get_result()
-                if arguments['environment']:
-                    res =""
-                    for key,val in arguments['environment'].items():
-                        res += (f'{key}={val}\n')
-                    res_bytes = res.encode('utf-8')
-                    self.assertEqual(
-                        env,
-                        res_bytes,
-                        "shell does not have the right env",
-                    )
-                if arguments['environment'] == {} or arguments['environment'] is None:
-                    self.assertEqual(
-                        env,
-                        b'',
-                        "shell does not have the right env",
+                    with timeout(5):
+                        execute_command(
+                                ["env"],
+                                ordered_output_hooks=output_hooks,
+                                ordered_input_hooks=input_hooks,
+                                **arguments,
+                            )
+                        env = result_hook_object.get_result()
+                        if arguments['environment']:
+                            res =""
+                            for key,val in arguments['environment'].items():
+                                res += (f'{key}={val}\n')
+                            res_bytes = res.encode('utf-8')
+                            self.assertEqual(
+                                env,
+                                res_bytes,
+                                "shell does not have the right env",
+                            )
+                        if arguments['environment'] == {} or arguments['environment'] is None:
+                            self.assertEqual(
+                                env,
+                                b'',
+                                "shell does not have the right env",
+                            )
+                except TestTimeout:
+                    self.fail(
+                        "execution timed out"
                     )
 
+    @unittest.skip("disabled for debugging")
     def test_dir(self) -> None:
         """Test to see if the correct directory is used when running commands"""
         def expected_full_path(path_lib:pathlib.Path):
             expected_path = os.getcwd()
+            print('----------------')
+            print(expected_path)
             path = str(path_lib)
+            print(path)
             while True:
                 if path.startswith('/'):
                     return path
-                if path.startswith('./'):
-                    path = path.removeprefix('./')
-                if path.startswith('../'):
+                elif path.startswith('../'):
                     path = path.removeprefix('../')
                     expected_path = expected_path[:expected_path.rindex('/')]
+                elif path.startswith('..'):
+                    path = path.removeprefix('..')
+                    expected_path = expected_path[:expected_path.rindex('/')]
+                elif path.startswith('./'):
+                    path = path.removeprefix('./')
+                elif path.startswith('.'):
+                    path = path.removeprefix('.')
                 else:
                     if path == '':
                         return expected_path
@@ -190,7 +210,8 @@ class BasicShellTests(unittest.TestCase):
         # standard arguments
         arguments_list = get_arguments_dict_list(
             {
-                'current_dir':[None, pathlib.Path(__file__).parent.resolve(),pathlib.Path('/')],
+                'current_dir':[None, pathlib.Path(__file__).parent.resolve(),pathlib.Path('/tmp'),pathlib.Path('./'),pathlib.Path('../../')],
+                # 'current_dir':[pathlib.Path('./'),pathlib.Path('../../')],
                 'environment':[None,{'test':'test'},{'a':'12','b':'11'},{}],
                 'timeout':[None,1,10,99999999999],
                 'ignore_ret_codes':[None,(1,),(1,7,),()],
@@ -200,28 +221,134 @@ class BasicShellTests(unittest.TestCase):
             # hook based argumens
             hooklist = generate_test_hook_lists(force_output=True)
             for input_hooks,output_hooks,result_hook_object in hooklist:
+                try:
+                    with timeout(5):
+                        # execution
+                        execute_command(
+                                ["pwd"],
+                                ordered_output_hooks=output_hooks,
+                                ordered_input_hooks=input_hooks,
+                                **arguments,
+                            )
+                        if arguments['current_dir'] is None:
+                            arguments['current_dir'] = './'
 
-                # execution
-                execute_command(
-                        ["pwd"],
-                        ordered_output_hooks=output_hooks,
-                        ordered_input_hooks=input_hooks,
-                        **arguments,
+                        curr_dir = result_hook_object.get_result()
+                        expected_path = expected_full_path(arguments['current_dir'])
+                        print('')
+                        print(curr_dir)
+                        print(f'{expected_path}\n'.encode('utf-8'))
+
+                        self.assertEqual(
+                            curr_dir,
+                            f'{expected_path}\n'.encode('utf-8'),
+                            "the paths do not match"
+                        )
+                except TestTimeout:
+                    self.fail(
+                        "execution timed out"
                     )
-                if arguments['current_dir'] is None:
-                    arguments['current_dir'] = './'
-
-                curr_dir = result_hook_object.get_result()
-                expected_path = expected_full_path(arguments['current_dir'])
-                self.assertEqual(
-                    curr_dir,
-                    f'{expected_path}\n'.encode('utf-8'),
-                    "the paths do not match"
-                )
 
 
+    @unittest.skip("disabled for debugging")
+    def test_ignore_ret_codes_are_ignored(self) -> None:
+        # standard arguments
+        arguments_list = get_arguments_dict_list(
+            {
+                'current_dir':[None, pathlib.Path(__file__).parent.resolve(),pathlib.Path('/')],
+                'environment':[None,{'test':'test'},{'a':'12','b':'11'},{}],
+                'timeout':[None,1,10,99999999999],
+                "ignore_ret_codes": [
+                    (),
+                    (1,4,),
+                    (2, 7,),
+                    (4,5,),
+                    (63,),
+                    (0,),
+                ],
+            }
+        )
+        for arguments in arguments_list:
+            # hook based argumens
+            hooklist = generate_test_hook_lists()
+            for input_hooks,output_hooks,_ in hooklist:
 
+                try:
+                    with timeout(20):
+                        retcode_to_output = (
+                            arguments["ignore_ret_codes"][len(arguments["ignore_ret_codes"]) - 1 % 3]
+                            if len(arguments["ignore_ret_codes"]) > 0
+                            else 0
+                        )
+                        command_process = execute_command(
+                            [script_path_string("returnExitCode")],
+                            ordered_output_hooks=output_hooks,
+                            ordered_input_hooks=input_hooks,
+                            **arguments,
+                            std_input=StringIOStream(f"{retcode_to_output}\n"),
+                        )
+                        retcode = command_process.get_return_code()
+                        self.assertEqual(retcode,
+                                         retcode_to_output,
+                                         "Mistake in the test wrong return code was returned by programm")
+                except TestTimeout:
+                    self.fail(
+                        "execution timed out"
+                    )
+                except CalledProcessError:
+                    self.fail(
+                        f"process trew an error with retcode {retcode_to_output} and ignored list {arguments["ignore_ret_codes"]}"
+                    )
 
+    # @unittest.skip("disabled for debugging")
+    def test_ignore_ret_codes_dont_ignore_other(self) -> None:
+        # standard arguments
+        arguments_list = get_arguments_dict_list(
+            {
+                'current_dir':[None, pathlib.Path(__file__).parent.resolve(),pathlib.Path('/')],
+                'environment':[None,{'test':'test'},{'a':'12','b':'11'},{}],
+                'timeout':[None,1,10,99999999999],
+                "ignore_ret_codes": [
+                    (),
+                    (2, 7,),
+                    (63,),
+                    (0,),
+                ],
+                "success_value": [0, 1, 53, 19],
+            }
+        )
+        for arguments in arguments_list:
+            # hook based argumens
+            hooklist = generate_test_hook_lists()
+            for input_hooks,output_hooks,_ in hooklist:
+                try:
+                    retcode_to_output = (
+                                3
+                                + arguments["success_value"]
+                                + (
+                                    arguments["ignore_ret_codes"][len(arguments["ignore_ret_codes"]) - 1 % 3]
+                                    if len(arguments["ignore_ret_codes"]) > 0
+                                    else arguments["success_value"]
+                                )
+                            )
+                    with self.assertRaises(CalledProcessError,
+                                           msg=f"process did not trow an error with retcode {retcode_to_output}, ignored list {arguments["ignore_ret_codes"]} and succes value {arguments['success_value']}"):
+                        with timeout(20):
+                            command_process = execute_command(
+                                [script_path_string("returnExitCode")],
+                                ordered_output_hooks=output_hooks,
+                                ordered_input_hooks=input_hooks,
+                                **arguments,
+                                std_input=StringIOStream(f"{retcode_to_output}\n"),
+                            )
+                            retcode = command_process.get_return_code()
+                            self.assertEqual(retcode,
+                                            retcode_to_output,
+                                            "Mistake in the test wrong return code was returned by programm")
+                except TestTimeout:
+                    self.fail(
+                        "execution timed out"
+                    )
 
 if __name__ == "__main__":
     unittest.main()
