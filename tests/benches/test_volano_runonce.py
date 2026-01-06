f7c24e165753687e9346b50e4bc2c98cd1572146
Mats Van Molle <mats.vanmolle@icloud.com>
2026-01-06 16:33:33 +0100

    WIP: Volano bench
    Signed-off-by: Mats Van Molle <mats.vanmolle@icloud.com>

---
+++ tests/benches/test_volano_runonce.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2026 Vrije Universiteit Brussel. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+import logging
+import subprocess
+import time
+from pathlib import Path
+
+from benchkit.benches.volano import VolanoBench
+from benchkit.engine.runonce import run_once
+from benchkit.utils.logging import bkpprint, bkprint, configure_logging
+
+
+def main() -> None:
+    configure_logging(
+        rich=True,
+        level=logging.DEBUG,
+        stdout_level=logging.INFO,
+        file_level=logging.DEBUG,
+    )
+
+    benchkit_home_dir = Path("~/.benchkit/").expanduser().resolve()
+    benches_dir = benchkit_home_dir / "benches"
+    results_dir = benchkit_home_dir / "results"
+
+    command = "./startup.sh server loop openjdk"
+
+    process = subprocess.Popen(
+        command,
+        shell=True,
+        cwd=benches_dir / "volano",
+    )
+
+    time.sleep(5)
+
+    result = run_once(
+        bench=VolanoBench(),
+        args={
+            "parent_dir": benches_dir,
+            "commit": "ac691084fdc5546421a55b25e7653d450e5a25fb",
+            "patches": [
+                Path("../../tutorials/leveldb-bench/patch.diff").resolve(),
+                Path("../../tutorials/leveldb-bench/patch02.diff").resolve(),
+            ],
+            "bench_name": "readrandom",
+            "nb_threads": 3,
+        },
+        duration_s=2,
+        record_dir=results_dir,
+    )
+
+    bkprint("Benchmark result:")
+    bkpprint(result)
+
+    process.terminate()
+
+
+if __name__ == "__main__":
+    main()
