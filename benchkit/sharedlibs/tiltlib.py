6d374dbfe58e8ec8519196485d95d3f5a701dd07
Antonio Paolillo <apaolill@gmail.com>
2026-01-27 14:15:57 +0100

    sharedlibs/tilt: Simplify preload lookup
    Move the Tilt shared-lib CMake configure/build helper into
    benchkit.sharedlibs.tiltlib and reuse it from the examples kit.
    
    Refactor TiltLib to build into a host-scoped build directory and enable
    locks by preloading lib<lock>.so from that build dir; drop legacy
    compiler/atomics/lock-path plumbing and raise a clear error if the
    requested lock lib is missing.
    
    examples/tilt: pin libvsync/tilt versions in setup README.
    
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- benchkit/sharedlibs/tiltlib.py
+++ benchkit/sharedlibs/tiltlib.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Huawei Technologies Co., Ltd. All rights reserved.
+# Copyright (C) 2024 Vrije Universiteit Brussel. All rights reserved.
 # SPDX-License-Identifier: MIT
 """
 Tilt shared library for drop-in replacement of pthread_mutex_t type and associated functions
@@ -6,166 +6,83 @@ Tilt shared library for drop-in replacement of pthread_mutex_t type and associat
 """
 
 import pathlib
-import re
-from typing import List, Optional, Tuple
+import sys
+from typing import Tuple
 
+from benchkit.platforms import Platform
 from benchkit.sharedlibs import (
     EnvironmentVariables,
     FromSourceSharedLib,
     LdPreloadLibraries,
-    PackageDependency,
 )
-from benchkit.shell.shell import shell_out
-from benchkit.utils.types import PathType
+
+
+def cmake_configure_build(
+    platform: Platform,
+    src_dir: pathlib.Path,
+    build_dir: pathlib.Path,
+    debug: bool,
+    make_suffix: str,
+) -> None:
+    platform.comm.makedirs(path=build_dir, exist_ok=True)
+
+    cmake_build_type = "Debug" if debug else "Release"
+    platform.comm.shell(
+        command=["cmake", f"-DCMAKE_BUILD_TYPE={cmake_build_type}", f"{src_dir}"],
+        current_dir=build_dir,
+        output_is_log=True,
+    )
+    platform.comm.shell(
+        command=f"make{make_suffix}",
+        current_dir=build_dir,
+        output_is_log=True,
+    )
 
 
 class TiltLib(FromSourceSharedLib):
-    """
-    Tilt shared library for drop-in replacement of pthread_mutex_t type and associated functions
-    """
-
-    class LibTiltNotFoundError(Exception):
-        """Error raised when tilt library is not found."""
-
     def __init__(
         self,
-        src_path: PathType,
-        debug_mode: bool,
+        tilt_locks_dir: pathlib.Path,
+        build_prefix: str = "build",
+        debug_mode: bool = False,
     ) -> None:
-        super().__init__(src_path=src_path, debug_mode=debug_mode)
+        super().__init__(src_path=tilt_locks_dir, debug_mode=debug_mode)
+        self.build_dir = self.src_path / f"{build_prefix}-{self.platform.hostname}"
 
-    def dependencies(self) -> List[PackageDependency]:
-        return super().dependencies()
+    def build(self) -> None:
+        cmake_configure_build(
+            platform=self.platform,
+            src_dir=self.src_path,
+            build_dir=self.build_dir,
+            debug=self._debug_mode,
+            make_suffix=" -j1",
+        )
 
-    def configure(self) -> None:
-        super().configure()
-
-    def clean(self) -> None:
-        """
-        Clean the library build.
-        """
-        raise NotImplementedError
-
-    def build_single_lock(
+    def preload(
         self,
-        lock: str,
-        atomics: str,
-        use_lse: bool,
-    ) -> None:
-        """
-        Build the library for the given lock.
-
-        Args:
-            lock (str):
-                the name of the lock to build.
-            atomics (str):
-                what atomics the lock is supposed to used.
-                Supported: a64 (for armv8), c11 (for std atomic), blt (for builtins).
-            use_lse (bool):
-                whether to use LSE instructions on Armv8.
-                TODO: not supported for now.
-        """
-        raise NotImplementedError
-
-    def get_compiler(self) -> str:
-        """
-        Return the compiler used to build the tilt library.
-
-        Returns:
-            str: the compiler used to build the tilt library.
-        """
-        cmake_logpath = self.build_dir / "CMakeFiles/CMakeOutput.log"
-        with open(cmake_logpath, "r") as cmake_logfile:
-            for line in cmake_logfile:
-                match = re.match(r"Compiler: (.*)\n", line)
-                if match is not None:
-                    break
-        compiler = match.groups()[0].strip()
-        return compiler
-
-    def get_exact_compiler(self) -> PathType:
-        """
-        Return the absolute path to the compiler used to build the tilt library.
-
-        Returns:
-            PathType: the absolute path to the compiler used to build the tilt library.
-        """
-        compiler = self.get_compiler()
-        which_compiler = shell_out(f"which {compiler}").strip()
-        exact_compiler = pathlib.Path(which_compiler).resolve()
-        return exact_compiler
-
-    def atomics_suffix(
-        self,
-        atomics: str,  # pylint: disable=unused-argument
-    ) -> str:
-        """
-        What is the suffix for the "atomics" part of the tilt library name.
-
-        Args:
-            atomics (str): atomics used to build the tilt library.
-
-        Returns:
-            str: the suffix to add to the library name.
-        """
-        # Notice that this has been deprecated.
-        return ""
-
-    def check_path_for_lock(
-        self,
-        lock: str,
-        use_lse: bool,  # pylint: disable=unused-argument
-        atomics: str,
-        lib_name: str,
-    ) -> Optional[PathType]:
-        """
-        Check that the expected path to the tilt library exists for the given lock.
-
-        Args:
-            lock (str): the lock to check.
-            use_lse (bool): whether LSE was enabled to build the lock (TODO not implemtend).
-            atomics (str): what atomics were used to build the lock.
-            lib_name (str): the name of the library.
-
-        Raises:
-            self.LibTiltNotFoundError: if the path to the lock cannot be reached.
-
-        Returns:
-            Optional[PathType]: the path to the tilt library.
-        """
-        if not lock:  # baseline
-            return None
-
-        atomics_suffix = self.atomics_suffix(atomics=atomics)
-        lock_path = self.build_dir / f"{lib_name}-{lock}{atomics_suffix}.so"
-        libtilt_path = lock_path.resolve()
-
-        if not libtilt_path.is_file():
-            raise self.LibTiltNotFoundError(f'Unknown path to libtilt: "{libtilt_path}"')
-        return libtilt_path
-
-    def preload(  # pylint: disable=arguments-differ
-        self,
-        lock: str,
-        use_lse: bool,
-        atomics: str,
-        lib_name: str,
         **kwargs,
     ) -> Tuple[LdPreloadLibraries, EnvironmentVariables]:
-        ld_preloads, other_env_vars = super().preload(
-            lock=lock,
-            use_lse=use_lse,
-            atomics=atomics,
+        ld_preloads, env_vars = super().preload(
             **kwargs,
         )
 
-        libtilt_path = self.check_path_for_lock(
-            lock=lock,
-            use_lse=use_lse,
-            atomics=atomics,
-            lib_name=lib_name,
-        )
-        if libtilt_path is not None:
-            ld_preloads.append(libtilt_path)
+        lock = kwargs.get("lock")
+        enabled = lock not in ["", "default", "baseline"]
+        if enabled:
+            lib_lockname = f"lib{lock}"
+            lib_path = (self.build_dir / f"{lib_lockname}.so").resolve()
+            if lib_path.is_file():
+                ld_preloads.append(str(lib_path))
+            else:
+                print(
+                    (
+                        f"[WARNING] Tilt lib lock with name {lib_lockname} not found "
+                        f"(in '{lib_path}'), not enabling."
+                    ),
+                    file=sys.stderr,
+                )
+                raise ValueError(
+                    f"Tilt lib lock with name {lib_lockname} not found in '{lib_path}'"
+                )
 
-        return ld_preloads, other_env_vars
+        return ld_preloads, env_vars
--- examples/tilt/kit/tiltlib.py
+++ examples/tilt/kit/tiltlib.py
@@ -2,8 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 import pathlib
-import sys
-from typing import Any, Dict, Iterable, List, Tuple
+from typing import Any, Dict, Iterable, List
 
 from benchkit.benchmark import (
     Benchmark,
@@ -14,80 +13,9 @@ from benchkit.benchmark import (
     SharedLib,
 )
 from benchkit.platforms import Platform
-from benchkit.sharedlibs import FromSourceSharedLib
+from benchkit.sharedlibs.tiltlib import cmake_configure_build
 from benchkit.utils.dir import caller_dir
-from benchkit.utils.types import EnvironmentVariables, LdPreloadLibraries, PathType
-
-
-def cmake_configure_build(
-    platform: Platform,
-    src_dir: pathlib.Path,
-    build_dir: pathlib.Path,
-    debug: bool,
-    make_suffix: str,
-) -> None:
-    platform.comm.makedirs(path=build_dir, exist_ok=True)
-
-    cmake_build_type = "Debug" if debug else "Release"
-    platform.comm.shell(
-        command=["cmake", f"-DCMAKE_BUILD_TYPE={cmake_build_type}", f"{src_dir}"],
-        current_dir=build_dir,
-        output_is_log=True,
-    )
-    platform.comm.shell(
-        command=f"make{make_suffix}",
-        current_dir=build_dir,
-        output_is_log=True,
-    )
-
-
-class TiltLib(FromSourceSharedLib):
-    def __init__(
-        self,
-        tilt_locks_dir: pathlib.Path,
-        build_prefix: str = "build",
-        debug_mode: bool = False,
-    ) -> None:
-        super().__init__(src_path=tilt_locks_dir, debug_mode=debug_mode)
-        self.build_dir = self.src_path / f"{build_prefix}-{self.platform.hostname}"
-
-    def build(self) -> None:
-        cmake_configure_build(
-            platform=self.platform,
-            src_dir=self.src_path,
-            build_dir=self.build_dir,
-            debug=self._debug_mode,
-            make_suffix=" -j1",
-        )
-
-    def preload(
-        self,
-        **kwargs,
-    ) -> Tuple[LdPreloadLibraries, EnvironmentVariables]:
-        ld_preloads, env_vars = super().preload(
-            **kwargs,
-        )
-
-        lock = kwargs.get("lock")
-        enabled = lock not in ["", "default", "baseline"]
-        if enabled:
-            lib_lockname = f"lib{lock}"
-            lib_path = (self.build_dir / f"{lib_lockname}.so").resolve()
-            if lib_path.is_file():
-                ld_preloads.append(str(lib_path))
-            else:
-                print(
-                    (
-                        f"[WARNING] Tilt lib lock with name {lib_lockname} not found "
-                        f"(in '{lib_path}'), not enabling."
-                    ),
-                    file=sys.stderr,
-                )
-                raise ValueError(
-                    f"Tilt lib lock with name {lib_lockname} not found in '{lib_path}'"
-                )
-
-        return ld_preloads, env_vars
+from benchkit.utils.types import PathType
 
 
 class SimpleMutexTestBench(Benchmark):
