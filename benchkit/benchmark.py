9e9a0f19ba4a932fe3144180f2f9905c039e47f6
estak <esteban.aguililla.klein.pro@outlook.com>
2026-01-28 14:37:50 +0100

    Move encoders to utils
    Signed-off-by: estak <esteban.aguililla.klein.pro@outlook.com>

--- benchkit/benchmark.py
+++ benchkit/benchmark.py
@@ -38,6 +38,7 @@ from benchkit.utils.types import (
     SplitCommand,
 )
 from benchkit.utils.variables import list_groupby
+from benchkit.utils.json_encoders import MultipleJsonEncoders, PathEncoder
 
 RecordKey = str
 RecordValue = Any
@@ -94,46 +95,6 @@ class CommandAttachment(Protocol):
         process: AsyncProcess,
         record_data_dir: PathType,
     ) -> None: ...
-
-
-class PathEncoder(json.JSONEncoder):
-    """
-    JSONEncoder extension to support pathlib.Path serialization.
-    """
-
-    def default(self, obj):
-        if isinstance(obj, pathlib.Path):
-            return str(obj)
-        return super().default(obj)
-
-
-class MultipleJsonEncoders:
-    """
-    Combine multiple JSON encoders
-    Source - https://stackoverflow.com/a
-    Posted by tsorn
-    Retrieved 2026-01-13, License - CC BY-SA 4.0
-    """
-
-    def __init__(self, encoders: list[Type[json.JSONEncoder]]) -> None:
-        self.encoders = encoders
-        self.args = ()
-        self.kwargs = {}
-
-    def default(self, obj):
-        for encoder in self.encoders:
-            try:
-                return encoder(*self.args, **self.kwargs).default(obj)
-            except TypeError:
-                pass
-        raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
-
-    def __call__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-        enc = json.JSONEncoder(*args, **kwargs)
-        enc.default = self.default
-        return enc
 
 
 class Benchmark:
---
+++ benchkit/utils/json_encoders.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2026 Vrije Universiteit Brussel. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+import json
+from typing import Type
+
+class PathEncoder(json.JSONEncoder):
+    """
+    JSONEncoder extension to support pathlib.Path serialization.
+    """
+
+    def default(self, obj):
+        if isinstance(obj, pathlib.Path):
+            return str(obj)
+        return super().default(obj)
+
+
+class MultipleJsonEncoders:
+    """
+    Combine multiple JSON encoders
+    Source - https://stackoverflow.com/a
+    Posted by tsorn
+    Retrieved 2026-01-13, License - CC BY-SA 4.0
+    """
+
+    def __init__(self, encoders: list[Type[json.JSONEncoder]]) -> None:
+        self.encoders = encoders
+        self.args = ()
+        self.kwargs = {}
+
+    def default(self, obj):
+        for encoder in self.encoders:
+            try:
+                return encoder(*self.args, **self.kwargs).default(obj)
+            except TypeError:
+                pass
+        raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
+
+    def __call__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        enc = json.JSONEncoder(*args, **kwargs)
+        enc.default = self.default
+        return enc
