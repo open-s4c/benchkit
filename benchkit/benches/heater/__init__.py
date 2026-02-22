3eac5dfa8825e4d2cd96510ed12916da0da67340
Antonio Paolillo <apaolill@gmail.com>
2026-02-22 16:04:53 +0100

    benches/heater: Export function in package init (#328)
    Align the heater benchmark package structure with the camera-ready
    version of the ICPE'26 paper by explicitly exporting
    `heater_seq_campaign` from `benchkit.benches.heater`.
    
    This makes the sequential heater campaign part of the public module
    interface and ensures that imports used in the paper (and in
    reproducibility scripts) match the documented API.
    
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- benchkit/benches/heater/__init__.py
+++ benchkit/benches/heater/__init__.py
@@ -1,2 +1,6 @@
 # Copyright (C) 2026 Vrije Universiteit Brussel. All rights reserved.
 # SPDX-License-Identifier: MIT
+
+from benchkit.benches.heater.sequential import heater_seq_campaign
+
+__all__ = ["heater_seq_campaign"]
