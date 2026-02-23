c4a96052ba9a28db2eacee2d0ccfdcccd4b94904
theodegeest <theo.degeest@gmail.com>
2026-02-23 21:06:16 +0100

    feat: monitor lock events on all threads using perf
    Signed-off-by: theodegeest <theo.degeest@gmail.com>

--- examples/rocksdb/campaign_rocksdb_speedup_stacks.py
+++ examples/rocksdb/campaign_rocksdb_speedup_stacks.py
@@ -8,12 +8,16 @@ Example of campaign script for RocksDB benchmark.
 from rocksdb import rocksdb_campaign
 
 from benchkit.campaign import CampaignSuite
+from benchkit.commandwrappers.perf import enable_non_sudo_perf
 from benchkit.commandwrappers.speedupstack import SpeedupStackWrapper
+from benchkit.platforms import get_current_platform
 from benchkit.utils.dir import get_curdir
 
 
 def main() -> None:
     """Main function of the campaign script."""
+    platform = get_current_platform()
+    enable_non_sudo_perf(comm_layer=platform.comm)
 
     rocksdb_src_dir = (get_curdir(__file__) / "deps/rocksdb/").resolve()
     libbpf_tools_dir = (get_curdir(__file__) / "deps/bcc/libbpf-tools/").resolve()
@@ -29,9 +33,9 @@ def main() -> None:
             # "multireadrandom",
             # "readwhilewriting",
         ],
-        nb_runs=1,
+        nb_runs=5,
         benchmark_duration_seconds=10,
-        nb_threads=[4, 8],
+        nb_threads=[1, 2, 4, 8],
         command_wrappers=([speedupstackwrapper] + speedupstackwrapper.command_wrappers()),
         command_attachments=speedupstackwrapper.command_attachments(),
         post_run_hooks=speedupstackwrapper.post_run_hooks(),
@@ -89,6 +93,14 @@ def main() -> None:
         plot_name="lineplot",
         x="nb_threads",
         y="perf_record_lock_total_wait_ns",
+        hue="bench_name",
+    )
+
+    suite.generate_graph(
+        title="Perf Record Lock Thread Avg",
+        plot_name="lineplot",
+        x="nb_threads",
+        y="perf_record_lock_thread_avg_wait_ns",
         hue="bench_name",
     )
 
