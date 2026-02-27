45f25490177a5dc584971ba6bc589c09c6541f31
theodegeest <theo.degeest@gmail.com>
2026-02-27 12:13:07 +0100

    feat: initial speedup stack graph implementation
    Signed-off-by: theodegeest <theo.degeest@gmail.com>

--- examples/rocksdb/campaign_rocksdb_speedup_stacks.py
+++ examples/rocksdb/campaign_rocksdb_speedup_stacks.py
@@ -9,6 +9,7 @@ from rocksdb import rocksdb_campaign
 
 from benchkit.campaign import CampaignSuite
 from benchkit.commandwrappers.speedupstack import SpeedupStackWrapper
+from benchkit.lwchart import time_transformation
 from benchkit.utils.dir import get_curdir
 
 
@@ -22,10 +23,14 @@ def main() -> None:
 
     campaign = rocksdb_campaign(
         src_dir=rocksdb_src_dir,
-        bench_name=["readrandom"],
+        bench_name=[
+            "readrandom",
+            "readmissing",
+        ],
         nb_runs=5,
-        benchmark_duration_seconds=3,
-        nb_threads=[2, 4, 8],
+        benchmark_duration_seconds=10,
+        nb_threads=[1, 2, 4, 8],
+        # nb_threads=[1, 2],
         command_wrappers=([speedupstackwrapper] + speedupstackwrapper.command_wrappers()),
         command_attachments=speedupstackwrapper.command_attachments(),
         post_run_hooks=speedupstackwrapper.post_run_hooks(),
@@ -38,45 +43,63 @@ def main() -> None:
     suite.print_durations()
     suite.run_suite()
 
-    suite.generate_graph(
-        title="Throughput",
-        plot_name="lineplot",
-        x="nb_threads",
-        y="throughput",
-        hue="bench_name",
-    )
+    # suite.generate_graph(
+    #     title="Throughput",
+    #     plot_name="lineplot",
+    #     x="nb_threads",
+    #     y="throughput",
+    #     hue="bench_name",
+    # )
+
+    # suite.generate_graph(
+    #     title="Klockstat",
+    #     plot_name="lineplot",
+    #     x="nb_threads",
+    #     y="klockstat_total_wait_ns",
+    #     hue="bench_name",
+    # )
+
+    # suite.generate_graph(
+    #     title="Offcputime",
+    #     plot_name="lineplot",
+    #     x="nb_threads",
+    #     y="offcputime_avg_micro_s",
+    #     hue="bench_name",
+    # )
+
+    # suite.generate_graph(
+    #     title="Llcstat",
+    #     plot_name="lineplot",
+    #     x="nb_threads",
+    #     y="llcstat_total_nr_misses",
+    #     hue="bench_name",
+    # )
+
+    # suite.generate_graph(
+    #     title="Strace",
+    #     plot_name="lineplot",
+    #     x="nb_threads",
+    #     y="strace_total_time_s",
+    #     hue="bench_name",
+    # )
 
     suite.generate_graph(
-        title="Klockstat",
-        plot_name="lineplot",
-        x="nb_threads",
-        y="klockstat_total_wait_ns",
-        hue="bench_name",
+        title="Speedup Stack",
+        plot_name="speedup-stack",
+        duration_transformation=time_transformation("s", "ns"),
+        speedup_stack_components={
+            "klockstat_total_wait_ns": _id,
+            "offcputime_avg_micro_s": time_transformation("us", "ns"),
+            "llcstat_total_nr_misses": _id,
+            "strace_total_time_s": time_transformation("s", "ns"),
+        },
+        constant_duration=True,
+        speed_metric="operations/second",
     )
 
-    suite.generate_graph(
-        title="Offcputime",
-        plot_name="lineplot",
-        x="nb_threads",
-        y="offcputime_avg_micro_s",
-        hue="bench_name",
-    )
 
-    suite.generate_graph(
-        title="Llcstat",
-        plot_name="lineplot",
-        x="nb_threads",
-        y="llcstat_total_nr_misses",
-        hue="bench_name",
-    )
-
-    suite.generate_graph(
-        title="Strace",
-        plot_name="lineplot",
-        x="nb_threads",
-        y="strace_total_time_s",
-        hue="bench_name",
-    )
+def _id(val: float):
+    return val
 
 
 if __name__ == "__main__":
