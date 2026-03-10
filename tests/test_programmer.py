d9d09c59b9c2cf98a021bbbcf9c93043326c89b5
estak <esteban.aguililla.klein.pro@outlook.com>
2026-03-10 15:53:39 +0100

    Fix formatting
    Signed-off-by: estak <esteban.aguililla.klein.pro@outlook.com>

--- tests/test_programmer.py
+++ tests/test_programmer.py
@@ -5,5 +5,7 @@ import pathlib
 
 if __name__ == "__main__":
     openocd = OpenOCDProgrammer.with_board(board="st_nucleo_l4", need_sudo=True)
-    openocd.flash(bin=pathlib.Path("blink.elf").resolve(), addr="0x08000000")
-    openocd.reset()
+    # openocd.flash(bin=pathlib.Path("blink.elf").resolve(), addr="0x08000000")
+    #openocd.stop()
+    openocd.start()
+    # openocd.reset()
